85b319a3d62d9eac19842f1f1b6c652256a4de7c
Logan Zehr <zehrl315@outlook.com>
2020-11-10 20:04:51 -0800

    Finished draft

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,88 @@
 console.log("Hello!")
 
-var calendarParentEl = $(".calendarParent > .calendarContainer");
+var calendarBlocksEl = $("#calendarParent > .calendarContainer");
+var eventData = [];
+
 
 // Set current day in header
 $("#currentDay").text(moment().format("dddd, MMMM YYYY"));
 
-// Handle coloring of event inputs
+// Handle coloring & updating of event inputs/text
+function setColors() {
 
-// for each container
+    for (let i = 0; i < calendarBlocksEl.length; i++) {
 
-for (let i = 0; i < calendarParentEl.length; i++) {
-    console.log("For loop!");
+        var eventTime = Number($(calendarBlocksEl[i]).children("span").attr("data-hour"));
+        var eventEl = $(calendarBlocksEl[i]).find(".eventInput");
 
-    var eventTime = $(calendarParentEl[i]).children("span").attr("data-hour")
-    var eventEl = $(calendarParentEl[i]).find(".eventInput")
-
-
-    console.log(`${eventTime} vs. moment=${moment().format("H")}`);
-
-    if (eventTime == moment().format("H")) {
-        eventEl.css("background-color", "red");
-    } else if (eventTime > moment().format("H")) {
-        eventEl.css("background-color", "lightBlue");
-    } else {
-        eventEl.css("background-color", "lightGray");
+        if (eventTime == moment().format("H")) {
+            eventEl.css("background-color", "lightgreen");
+        } else if (eventTime > moment().format("H")) {
+            eventEl.css("background-color", "lightBlue");
+        } else {
+            eventEl.css("background-color", "lightGray");
+        }
     }
 }
 
+// Setup initial javascript object for storing calendar data
+function initializeData() {
+
+    var hour;
+    var text;
+
+    // If we don't have data stored
+    if (localStorage.getItem("eventData") === null) {
+
+        for (let i = 0; i < calendarBlocksEl.length; i++) {
+            hour = $(calendarBlocksEl.get(i)).children(".time").attr("data-hour");
+            text = ""
+
+            eventData.push({
+                hour,
+                text
+            });
+
+            console.log(hour, text)
+        } 
+
+        localStorage.setItem("eventData", JSON.stringify(eventData));
+    } else {textHistoryUpdate()}
+}
+
+// Pull local storage and populate on calendar
+function textHistoryUpdate() {
+    console.log("I found history! Time to update...")
+    eventData = JSON.parse(localStorage.getItem("eventData"));
+
+    // Set each input to the local storage text
+    for (let i = 0; i < calendarBlocksEl.length; i++) {
+        $(calendarBlocksEl.get(i)).find(".eventInput").val(eventData[i].text);
+    } 
+
+}
 
 // Handle saving and storage of event data
+function save(event) {
+    event.preventDefault();
 
+    // Get the target div
+    var timeSlotDivEl = $(event.target).parent(".calendarContainer");
 
+    // Get the array number that you are referencing
+    var arrayPos = calendarBlocksEl.index(timeSlotDivEl);
+
+    // Save text from div child to local storage
+    var eventDataStorage = JSON.parse(localStorage.getItem("eventData"));
+    eventDataStorage[arrayPos].text = timeSlotDivEl.find(".eventInput").val();
+    localStorage.setItem("eventData", JSON.stringify(eventDataStorage));
+
+}
+
+// Perform these Functions at the Beginning
+initializeData();
+setColors();
 
 // Event Handlers
+$("#calendarParent").on("click", ".icon", save);
+$("#calendarParent").submit(save);
